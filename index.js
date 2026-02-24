eb5273990cb5bafd597ecedee4cdc4068ff89858
Ryan5473 <rayenbelkahla219@gmail.com>
2026-02-24 15:28:10 +0100

    first commit

--- index.js
+++ index.js
@@ -3,38 +3,58 @@ import moment from "moment";
 import simpleGit from "simple-git";
 import random from "random";
 
+const git = simpleGit();
 const path = "./data.json";
 
-const markCommit = (x, y) => {
-  const date = moment()
-    .subtract(1, "y")
-    .add(1, "d")
-    .add(x, "w")
-    .add(y, "d")
-    .format();
+const makeCommit = async (date) => {
+  const data = { date };
 
-  const data = {
-    date: date,
-  };
+  await jsonfile.writeFile(path, data);
 
-  jsonfile.writeFile(path, data, () => {
-    simpleGit().add([path]).commit(date, { "--date": date }).push();
-  });
+  await git.add([path]);
+  await git.commit(date, { "--date": date });
 };
 
-const makeCommits = (n) => {
-  if(n===0) return simpleGit().push();
-  const x = random.int(0, 54);
-  const y = random.int(0, 6);
-  const date = moment().subtract(1, "y").add(1, "d").add(x, "w").add(y, "d").format();
+const generateCommits = async () => {
+  const startDate = moment("2022-01-01");
+  const endDate = moment("2022-12-31");
 
-  const data = {
-    date: date,
-  };
-  console.log(date);
-  jsonfile.writeFile(path, data, () => {
-    simpleGit().add([path]).commit(date, { "--date": date },makeCommits.bind(this,--n));
-  });
+  const totalDays = endDate.diff(startDate, "days");
+
+  for (let i = 0; i <= totalDays; i++) {
+    const currentDay = startDate.clone().add(i, "days");
+
+    const dayOfWeek = currentDay.day();
+
+    // 45% chance to skip day
+    if (random.float() < 0.45) continue;
+
+    let commitsToday;
+
+    if (dayOfWeek === 0 || dayOfWeek === 6) {
+      // Weekend
+      commitsToday = random.int(0, 2);
+    } else {
+      // Weekday
+      commitsToday = random.int(1, 4);
+    }
+
+    for (let j = 0; j < commitsToday; j++) {
+      const commitDate = currentDay
+        .clone()
+        .hour(random.int(9, 21))
+        .minute(random.int(0, 59))
+        .second(random.int(0, 59))
+        .format();
+
+      console.log("Commit:", commitDate);
+
+      await makeCommit(commitDate);
+    }
+  }
+
+  await git.push();
+  console.log("Done ✅ 2022 commits generated");
 };
 
-makeCommits(100);
+generateCommits();
